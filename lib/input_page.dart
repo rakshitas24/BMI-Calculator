e53cefe9deb285e6005176be9db27763c64e7be8
Rakshita Singh <rakshita.vorphy@gmail.com>
2022-09-30 10:58:08 +0530

    Gestures added on gender cards

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
 
 class InputPage extends StatefulWidget {
   @override
@@ -6,6 +12,27 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+Color maleCardColour = inactiveCardColour;
+Color femaleCardColour = inactiveCardColour;
+
+void updateColour(int gender)
+{
+  if(gender==1)
+  {
+    if(maleCardColour==inactiveCardColour)
+    maleCardColour=activeCardColour;
+    else maleCardColour=inactiveCardColour;
+  }
+  if(gender==2)
+  {
+    if(femaleCardColour==inactiveCardColour)
+    femaleCardColour=activeCardColour;
+    else femaleCardColour=inactiveCardColour;
+  }
+  
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,20 +46,62 @@ class _InputPageState extends State<InputPage> {
               child: Row(
             children: <Widget>[
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Color(0xFF1D1E33),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      updateColour(1);
+                    });
+                  },
+                  child: ReusableCard(
+                    colour: maleCardColour,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Color(0xFF1D1E33),
+                child: GestureDetector(
+                  onTap: () {
+                    updateColour(2);
+                  },
+                  child: ReusableCard(
+                    colour: femaleCardColour,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.venus,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'FEMALE',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -42,11 +111,27 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Color(0xFF1D1E33),
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'HEIGHT',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        ),
+                        Text(
+                          '150cm',
+                          style: TextStyle(
+                            fontSize: 50.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
@@ -56,28 +141,120 @@ class _InputPageState extends State<InputPage> {
           Expanded(
               child: Row(
             children: <Widget>[
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Color(0xFF1D1E33),
-                  ),
+              Expanded(child: ReusableCard(
+                colour: activeCardColour,
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'WEIGHT',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Color(0xFF8D8E98),
+                      ),
+                    ),
+                    Text(
+                      '87',
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.remove,
+                          size: 50.0,
+                        ),
+                        Icon(
+                          Icons.add,
+                          size: 50.0,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
                 ),
               ),
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Color(0xFF1D1E33),
-                  ),
+              Expanded(child: ReusableCard(
+                colour: activeCardColour,
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'AGE',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Color(0xFF8D8E98),
+                      ),
+                    ),
+                    Text(
+                      '25',
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.remove,
+                          size: 50.0,
+                        ),
+                        Icon(
+                          Icons.add,
+                          size: 50.0,
+                        )
+                      ],
+                    ),
+                  ],
                 ),
-              ),
+                ),
+                ),
             ],
           )),
+          Container(
+            color: Color(0xFFEB1555),
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: 80.0,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: Text(
+                'CALCULATE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          )
         ],
       )),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.colour, required this.cardChild});
+  Widget cardChild;
+  Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: colour,
+      ),
     );
   }
 }
